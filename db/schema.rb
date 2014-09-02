6ffd10b74bbc8e26f1580527e5fcc17d3c4245fd
Ashley <a.clarkem@gmail.com>
2014-09-02 14:24:44 -0600

    added completed column to todos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827224139) do
+ActiveRecord::Schema.define(version: 20140902202134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: true do |t|
-    t.string "todo"
+    t.string  "todo"
+    t.boolean "completed"
   end
 
 end
